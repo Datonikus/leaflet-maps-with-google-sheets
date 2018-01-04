fa59c21678f3b1049fa69341ca037f17d6ad84b7
caribeño <Datonikus@users.noreply.github.com>
2018-01-04 15:41:45 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1aLtaj0SWVBe8knY-QnFqJcagLgM0wvecPZcr9fjlzGc/edit#gid=0';
